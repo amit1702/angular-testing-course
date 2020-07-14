b6da29969d8ff34a4accee775d9a07a8a1d86a07
Amit Kumar <amiku.cse@gmail.com>
2020-07-14 19:24:00 +0530

    Added test for FindCourseById

--- src/app/courses/services/course.service.spec.ts
+++ src/app/courses/services/course.service.spec.ts
@@ -33,4 +33,18 @@ describe('CourseService', () => {
     expect(req.request.method).toEqual('GET');
     req.flush({payload: Object.values(COURSES)});
   });
+
+  it('should retrieve course with id 12', () => {
+    courseService.findCourseById(12)
+      .subscribe(course => {
+        expect(course).toBeTruthy();
+        expect(course.id).toBe(12);
+      });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('GET');
+    req.flush(COURSES[12]);
+  });
+
+
 });
