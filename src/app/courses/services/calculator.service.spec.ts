6033448ace0a392972bc516e56ccf3ed3087b77a
Amit Kumar <amiku.cse@gmail.com>
2020-07-14 17:25:11 +0530

    Adding beforeeach and TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,15 +1,29 @@
 import {LoggerService} from './logger.service';
 import {CalculatorService} from './calculator.service';
+import {TestBed} from '@angular/core/testing';
 
 describe('CalculatorService', () => {
+  let loggerSpy: any;
+  let calculator: CalculatorService;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    // calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    });
+    calculator = TestBed.get(CalculatorService);
+  });
+
   it('should add 2 numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
     expect(calculator.add(2, 2)).toBe(4, 'Unexpected addition result');
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract 2 numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     expect(calculator.subtract(2, 2)).toBe(0, 'Unexpected subtraction result');
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
