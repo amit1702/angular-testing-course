025c704cfcd9329166bfdf8641b48fd9ff689cca
Amit Kumar <amiku.cse@gmail.com>
2020-07-14 14:13:40 +0530

    Add spy on actual logger instance

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,8 +3,11 @@ import {CalculatorService} from './calculator.service';
 
 describe('CalculatorService', () => {
   it('should add 2 numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+    spyOn(logger, 'log');
+    const calculator = new CalculatorService(logger);
     expect(calculator.add(2, 2)).toBe(4, 'Unexpected addition result');
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract 2 numbers', () => {
     const calculator = new CalculatorService(new LoggerService());
