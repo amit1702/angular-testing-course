afe6148fda0dbc633c8e5904216035d97214e6f9
Amit Kumar <amiku.cse@gmail.com>
2020-07-14 14:25:14 +0530

    Provide jasmine created custom mocked object

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,8 +3,7 @@ import {CalculatorService} from './calculator.service';
 
 describe('CalculatorService', () => {
   it('should add 2 numbers', () => {
-    const logger = new LoggerService();
-    spyOn(logger, 'log');
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
     const calculator = new CalculatorService(logger);
     expect(calculator.add(2, 2)).toBe(4, 'Unexpected addition result');
     expect(logger.log).toHaveBeenCalledTimes(1);
