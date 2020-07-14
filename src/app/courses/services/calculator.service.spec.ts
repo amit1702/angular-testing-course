5419c8e0ae375db32a055f885d38a42d498ddd10
Amit Kumar <amiku.cse@gmail.com>
2020-07-14 14:02:02 +0530

    Completed my first test

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,8 +1,13 @@
+import {LoggerService} from './logger.service';
+import {CalculatorService} from './calculator.service';
+
 describe('CalculatorService', () => {
   it('should add 2 numbers', () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+    expect(calculator.add(2, 2)).toBe(4, 'Unexpected addition result');
   });
   it('should subtract 2 numbers', () => {
-    fail();
+    const calculator = new CalculatorService(new LoggerService());
+    expect(calculator.subtract(2, 2)).toBe(0, 'Unexpected subtraction result');
   });
 });
